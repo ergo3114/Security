27cafccb4b33893b6295bba1e6dc23cdc159ecf2
C. David Littlejohn <cdlittlejohn1@gmail.com>
2021-08-12 15:45:20 -0400

    Updated comment based help for Get-SNMPInfo

--- Get-SNMPInfo.ps1
+++ Get-SNMPInfo.ps1
@@ -1,27 +1,15 @@
 <#
 .SYNOPSIS
-    Brute forces a list of web site directories and returns the ones that are successful
+    Performs an SNMP query of a device.
 .DESCRIPTION
-    Queries a provided list file for directories against a provided URL. Returns the
-    HTTP Status code of each result. The list file does not have to have extensions
-    such as .asp or .html as the script will enumerate them.
-.PARAMETER URL
-    The URL for the target. Provide the full http://www.* or https://www.* for best results.
-.PARAMETER PathList
-    The list of directories to tack onto the end of the URL.
-.PARAMETER ShowNonHits
-    A switch parameter that; by default, filters our directory queries that did not provide
-    infomation. Provide this parameter to show the attempts that did not return an HTTP Status
-    Code.
-.PARAMETER AllowAutoRedirect
-    A switch parameter that; by default, restricts the HTTP Request from redirecting. Provide
-    this paramter to show the redirects and their URLS.
+    Creates a new COMObject of type olePrn.OleSNMP. Opens a connection to the given device and
+    queries predefined SNMP OIDs.
+.PARAMETER Device
+    The IP Address of the device in which you are wanting to retrieve SNMP information.
+.PARAMETER CommunityString
+    The passphrase the device is expecting to receive to grant access to SNMP information.
 .EXAMPLE
-    ScanDir -URL 'http://www.google.com/' -List '.\dirpath.list'
-.EXAMPLE
-    ScanDir -URL 'http://www.google.com/' -List '.\dirpath.list' -AllowsAutoRedirect
-.EXAMPLE
-    ScanDir -URL 'http://www.google.com/' -List '.\dirpath.list' -ShowNonHits
+    Get-SNMPInfo -Device 192.168.1.8 -CommunityString public
 .OUTPUTS
     PSCUSTOMOBJECT
 .NOTES
