2c8d48ac4583b9e7516c7e8c69dc15b897c0e82e
C. David Littlejohn <cdlittlejohn1@gmail.com>
2021-08-12 16:02:08 -0400

    Converted to advanced function

--- Get-SNMPInfo.ps1
+++ Get-SNMPInfo.ps1
@@ -16,6 +16,7 @@
     Author: ergo
 #>
 function Get-SNMPInfo{
+    [cmdletBinding()]
     Param(
         [Parameter(Mandatory=$true)]
         [Alias('Host')]
@@ -26,22 +27,28 @@ function Get-SNMPInfo{
         [string]
         $CommunityString
     )
-    $SNMP = New-object -ComObject olePrn.OleSNMP
-    $SNMP.open($Device,$CommunityString,2,1000)
-    $sysDescr = $SNMP.get('.1.3.6.1.2.1.1.1.0')
-    $contact = $SNMP.get('.1.3.6.1.2.1.1.4.0')
-    $sysname = $SNMP.get('.1.3.6.1.2.1.1.5.0')
-    $location = $SNMP.get('.1.3.6.1.2.1.1.6.0')
-    $uptime = $SNMP.get('.1.3.6.1.2.1.25.1.1.0')
-    $SNMP.Close()
-    $obj = [pscustomobject]@{
-        SysName = $sysname
-        Description = $sysDescr
-        Location = $location
-        Contact = $contact
-        UpTime = "$([math]::Round($($uptime/8640000),2)) days"
+    BEGIN{
+        $SNMP = New-object -ComObject olePrn.OleSNMP
+        $SNMP.open($Device,$CommunityString,2,1000)
     }
-    $result = New-Object System.Collections.ArrayList
-    $null = $result.Add($obj)
-    $result
+    PROCESS{
+        $sysDescr = $SNMP.get('.1.3.6.1.2.1.1.1.0')
+        $contact = $SNMP.get('.1.3.6.1.2.1.1.4.0')
+        $sysname = $SNMP.get('.1.3.6.1.2.1.1.5.0')
+        $location = $SNMP.get('.1.3.6.1.2.1.1.6.0')
+        $uptime = $SNMP.get('.1.3.6.1.2.1.25.1.1.0')
+    }
+    END{
+        $SNMP.Close()
+        $obj = [pscustomobject]@{
+            SysName = $sysname
+            Description = $sysDescr
+            Location = $location
+            Contact = $contact
+            UpTime = "$([math]::Round($($uptime/8640000),2)) days"
+        }
+        $result = New-Object System.Collections.ArrayList
+        $null = $result.Add($obj)
+        $result
+    }
 }
